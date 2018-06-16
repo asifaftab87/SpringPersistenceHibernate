12164154ebc058a52b83473f73d3853e4a521992
asifaftab87 <asifaftab87@gmail.com>
2018-06-16 10:49:06 +0800

    Adding extension .do

--- SecurityExample/src/main/java/org/liferayasif/security/controller/SecurityWebController.java
+++ SecurityExample/src/main/java/org/liferayasif/security/controller/SecurityWebController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class SecurityWebController {
 
-	@RequestMapping(method=RequestMethod.GET, value="/login")
+	@RequestMapping(method=RequestMethod.GET, value="/login.do")
 	public String home() {
 		return "view";
 	}
