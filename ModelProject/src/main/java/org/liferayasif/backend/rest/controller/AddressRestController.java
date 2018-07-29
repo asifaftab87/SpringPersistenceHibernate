fb734df39d0ac0d70c50699108433083f85cc4b9
Md Atif Aftab <atifaftab001@gmail.com>
2018-07-30 01:14:00 +0530

    List is created

--- ModelProject/src/main/java/org/liferayasif/backend/rest/controller/AddressRestController.java
+++ ModelProject/src/main/java/org/liferayasif/backend/rest/controller/AddressRestController.java
@@ -1,5 +1,30 @@
 package org.liferayasif.backend.rest.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.liferayasif.backend.model.Address;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/address")
 public class AddressRestController {
 
+	@RequestMapping(value="/listOfAddress")
+	public List<Address> AddressList(){
+		List<Address> addressList = new ArrayList<Address>();
+		Address address = new Address();
+
+		
+		address.setAdd1("Rifle Range Road");
+		address.setAdd2("Tiljala Road");
+		address.setAdd3("Shamshul huda Road");
+
+		addressList.add(address);
+		
+		return addressList;
+	}
+	
+	
 }
