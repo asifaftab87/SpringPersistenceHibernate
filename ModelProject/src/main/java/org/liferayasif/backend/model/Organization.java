83fccc4f96e193d7736ee9f1bd6ebcd4ef311571
ahmed reza <rezaahmed1986@gmail.com>
2018-07-30 14:57:42 +0530

    creaated org class with mem var companyid, userid, name,  addressid, country, city state

---
+++ ModelProject/src/main/java/org/liferayasif/backend/model/Organization.java
@@ -0,0 +1,15 @@
+package org.liferayasif.backend.model;
+
+public class Organization {
+	
+	//companyid, userid, name, addressid, country, city state
+	private int companyId;
+	private int userId;
+	private String name;
+	private String address;
+	private String country;
+	private String city;
+	private String state;
+	
+	
+}
