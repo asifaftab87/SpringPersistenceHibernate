0df525df010e54d5700b73d326cd7db8fbba054f
ahmed reza <rezaahmed1986@gmail.com>
2018-07-31 15:13:28 +0530

    Added interface

---
+++ ModelProject/src/main/java/org/liferayasif/backend/dao/WiproDao.java
@@ -0,0 +1,24 @@
+package org.liferayasif.backend.dao;
+
+import java.util.List;
+
+import org.liferayasif.backend.model.Wipro;
+
+
+
+public interface WiproDao {
+	
+	public List<Wipro> getAllWipros() ;
+	
+	public Wipro getWiproById(Integer id);
+	
+	public void adddWipro(Wipro wipro);
+	
+	public void deleteWipro(Integer id);
+	
+	
+	
+
+	
+	
+}
