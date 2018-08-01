de125c0323b97d97776236d85b6b9a2b7ce26782
ahmed reza <rezaahmed1986@gmail.com>
2018-08-01 09:51:13 +0530

    added method in serviceImpl

--- ModelProject/src/main/java/org/liferayasif/backend/service/impl/WiproServiceImpl.java
+++ ModelProject/src/main/java/org/liferayasif/backend/service/impl/WiproServiceImpl.java
@@ -1,5 +1,40 @@
 package org.liferayasif.backend.service.impl;
 
-public class WiproServiceImpl {
+import java.util.List;
 
+import org.liferayasif.backend.dao.WiproDao;
+import org.liferayasif.backend.model.Wipro;
+import org.liferayasif.backend.service.WiproService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class WiproServiceImpl implements WiproService {
+
+	@Autowired
+	private WiproDao wiproDao;
+
+	@Override
+	public List<Wipro> getAllWipros() {
+		List<Wipro> wiproList = wiproDao.getAllWipros();
+		return wiproList;
+	}
+
+	@Override
+	public Wipro getWiproById(Integer id) {
+		Wipro wipro=wiproDao.getWiproById(id);
+		return wipro;
+	}
+
+	@Override
+	public void adddWipro(Wipro wipro) {
+		wiproDao.adddWipro(wipro);
+		
+	}
+
+	@Override
+	public void deleteWipro(Integer id) {
+		wiproDao.deleteWipro(id);
+		
+	}
+	
+	
 }
