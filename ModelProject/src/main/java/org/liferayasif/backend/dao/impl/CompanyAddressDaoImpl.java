c144c1bc8f744a40ee417587697a16f8f059ce9b
ahmed reza <rezaahmed1986@gmail.com>
2018-08-03 02:05:57 +0530

    added companyaddress

---
+++ ModelProject/src/main/java/org/liferayasif/backend/dao/impl/CompanyAddressDaoImpl.java
@@ -0,0 +1,64 @@
+package org.liferayasif.backend.dao.impl;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.criterion.Restrictions;
+import org.liferayasif.backend.abstrct.dao.AbstractDao;
+import org.liferayasif.backend.dao.CompanyAddressDao;
+import org.liferayasif.backend.model.CompanyAddress;
+import org.springframework.stereotype.Repository;
+
+@Repository("companyAddressDao")
+
+public class CompanyAddressDaoImpl extends AbstractDao<Integer , CompanyAddress> implements CompanyAddressDao{
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<CompanyAddress> getAllCompanyAddresses() {
+		Criteria criteria = createEntityCriteria();
+		return (List<CompanyAddress>) criteria.list();
+	}
+
+	@Override
+	public CompanyAddress getCompanyAddressById(Integer id) {
+		
+		return getByKey(id);
+	}
+
+	@Override
+	public void addCompanyAddress(CompanyAddress companyAddress) {
+		persist(companyAddress);
+		
+	}
+
+	@Override
+	public void deleteCompanyAddress(Integer id) {
+		Query query = getSession().createSQLQuery("delete from CompnayAddress where id = :id");
+		query.setInteger("id", id);
+		query.executeUpdate();
+	}
+
+	@Override
+	public CompanyAddress updateCompanyAddress(CompanyAddress companyAddress) {
+		getSession().update(companyAddress);
+		return companyAddress;
+	}
+
+	@Override
+	public List<CompanyAddress> getCompanyAddressByUserId(Integer id) {
+
+		Criteria criteria = getSession().createCriteria(CompanyAddress.class);
+		criteria.add(Restrictions.eq("userId" , id));
+		
+		List<CompanyAddress> companyAddressList = criteria.list();
+		return companyAddressList;
+		
+	}
+
+	
+	
+	
+
+}
---
+++ ModelProject/src/main/java/org/liferayasif/backend/service/impl/CompanyAddressServiceImpl.java
@@ -0,0 +1,70 @@
+package org.liferayasif.backend.service.impl;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.liferayasif.backend.dao.CompanyAddressDao;
+import org.liferayasif.backend.model.CompanyAddress;
+import org.liferayasif.backend.service.CompanyAddressService;
+import org.liferayasif.backend.util.time.TimeUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("CompanyAddressService")
+@Transactional
+public class CompanyAddressServiceImpl implements CompanyAddressService {
+
+	@Autowired
+	private CompanyAddressDao companyAddressDao;
+	
+	@Override
+	public List<CompanyAddress> getAllCompanyAddresses() {
+		List<CompanyAddress> companyAddressList = companyAddressDao.getAllCompanyAddresses();
+		return companyAddressList;
+	}
+
+	@Override
+	public CompanyAddress getCompanyAddressById(Integer id) {
+		CompanyAddress companyAddress = companyAddressDao.getCompanyAddressById(id);
+		return companyAddress;
+	}
+
+	@Override
+	public void addCompanyAddress(CompanyAddress companyAddress) {
+		companyAddress.setCreateDt(TimeUtil.getSqlTimStmp());
+		companyAddress.setUpdateDt(TimeUtil.getSqlTimStmp());
+		companyAddressDao.addCompanyAddress(companyAddress);
+	}
+
+	@Override
+	public void addCompanyAddressList(List<CompanyAddress> companyAddressList) {
+		if(companyAddressList !=null && companyAddressList.size()>0){
+			
+			for(CompanyAddress companyAddress : companyAddressList){
+				addCompanyAddress(companyAddress);
+				
+			}
+		}
+		
+	}
+
+	@Override
+	public void deleteCompanyAddress(Integer id) {
+		companyAddressDao.deleteCompanyAddress(id);
+		
+	}
+
+	@Override
+	public CompanyAddress updateCompanyAddress(CompanyAddress companyAddress) {
+		
+		return companyAddressDao.updateCompanyAddress(companyAddress);
+	}
+
+	@Override
+	public List<CompanyAddress> getCompanyAddressesByCompaanyId(Integer id) {
+		List<CompanyAddress> companyAddressList =companyAddressDao.getCompanyAddressByUserId(id);
+		return companyAddressList;
+	}
+
+}
