b7b0eb396a8ec47438a786df54c05b6c3f3b7adb
ahmed reza <rezaahmed1986@gmail.com>
2018-08-04 19:17:06 +0530

    ADDED COMMAND TO ADD TABLE

--- ModelProject/src/main/java/org/liferayasif/backend/model/CompanyAddress.java
+++ ModelProject/src/main/java/org/liferayasif/backend/model/CompanyAddress.java
@@ -4,10 +4,13 @@ import java.io.Serializable;
 import java.sql.Timestamp;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+import javax.persistence.Table;
+@Entity
+@Table(name="COMPANY_ADDRESS")
 public class CompanyAddress implements Serializable {
 
 	
