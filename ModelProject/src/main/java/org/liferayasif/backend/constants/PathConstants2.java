3343078fd5b285fdeab8d157047a6e2afea77c12
ahmed reza <rezaahmed1986@gmail.com>
2018-08-08 13:42:53 +0530

    deleted constant2

--- ModelProject/src/main/java/org/liferayasif/backend/constants/PathConstants2.java
+++
@@ -1,9 +0,0 @@
-package org.liferayasif.backend.constants;
-
-public interface PathConstants2 {
-
-	String USER 										= "/Comp";
-	String FIND_BY_ID 									= "/findById";
-	String ADD_USER 									= "/addUser";
-	String USER_ADD										="/userAdd";
-}
