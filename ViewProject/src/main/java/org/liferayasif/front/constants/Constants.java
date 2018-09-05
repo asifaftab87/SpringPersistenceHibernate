4b39273db34d7b5a6831100039fc8cc96b133591
ahmed reza <rezaahmed1986@gmail.com>
2018-09-05 15:50:26 +0530

    constant added

--- ViewProject/src/main/java/org/liferayasif/front/constants/Constants.java
+++ ViewProject/src/main/java/org/liferayasif/front/constants/Constants.java
@@ -6,5 +6,8 @@ public class Constants {
 	public static final String VIEW_PREFIX_PATH		= "/WEB-INF/views/";
 	public static final String VIEW_SUFFIX_PATH		= ".jsp";
 	public static final String BASE_PACKAGE			= "org.liferayasif.front";
+	public static final String WORKER				= "/worker";
+	public static final String ADD_OBJ				= "/addObj";
+	
 	 
 }
