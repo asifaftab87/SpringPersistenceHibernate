a160453ed77010427a72732625d959ae46e207a9
Md Atif Aftab <atifaftab001@gmail.com>
2018-09-07 20:10:55 +0530

    removed conflicts

--- ViewProject/src/main/webapp/static/script/main.js
+++ ViewProject/src/main/webapp/static/script/main.js
@@ -1,30 +1,34 @@
 $(document).ready(function(){
 	 $( "#birthDate" ).datepicker();
-	 
+
 	 try{
 		var x = document.querySelectorAll(".navTab");
-			
-		for(var i=0; i<x.length; i++){
-			$(x[i]).attr("class", "navTab");
-		}
-	 }
-	 catch(err){
-		 
-	 }
-	 
-	 var curRootPage = $('#curRootPage').val(); 
-	 $('#'+curRootPage).attr('class', 'navTab active');
-	 
-});
-
-function makeAtcive(obj){
-	console.log("asif new changes");
-	console.log("d: "+$('#birthDate').val());
-/*	var x = document.querySelectorAll(".navTab");
 	
 	for(var i=0; i<x.length; i++){
 		$(x[i]).attr("class", "navTab");
 	}
-    
+}
+	 catch(err){
+
+		}
+		
+	 var curRootPage = $('#curRootPage').val(); 
+	 $('#'+curRootPage).attr('class', 'navTab active');
+		
+});
+		
+function makeAtcive(obj){
+	console.log("asif new changes");
+	console.log("d: "+$('#birthDate').val());
+/*	var x = document.querySelectorAll(".navTab");
+			
+	for(var i=0; i<x.length; i++){
+		$(x[i]).attr("class", "navTab");
+		}
+		
 	$(obj).attr("class", "navTab active");*/
 }
+function atifChnaged(){
+	console.log("sdfasdf");
+}
+	
